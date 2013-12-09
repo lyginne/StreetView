e98dfe30e18815913e298d44dae255a3896a238c
Nikitos <nikitos1810@gmail.com>
2013-12-10 01:22:14 +0400

    migrate

--- StreetView/OpenGL/Controls/StreetViewControl.cs
+++ StreetView/OpenGL/Controls/StreetViewControl.cs
@@ -10,42 +10,115 @@ using StreetView.OpenGL.StreetElements;
 using Rectangle = System.Drawing.Rectangle;
 
 namespace StreetView.OpenGL.Controls
-{   public class StreetViewControl : OpenGLControl
+{   
+    public class StreetViewControl : OpenGLControl
     {
-
-    public void DrawBackground()
-    {
-        GL.glMatrixMode(GL.GL_PROJECTION);
-        GL.glPushMatrix();
-        GL.glOrtho(0, 1, 0, 1, 0, 1);
-
-        GL.glMatrixMode(GL.GL_MODELVIEW);
-        GL.glPushMatrix();
-        GL.glLoadIdentity();
-
-        // No depth buffer writes for background.
-        GL.glDepthMask(0);
-
-        GL.glBindTexture(GL.GL_TEXTURE_2D, Textures.SkyTexture.TextureBytes[0]);
-        GL.glBegin(GL.GL_QUADS);
+        public void InitShadows()
         {
-            GL.glTexCoord2f(0f, 0f);
-            GL.glVertex2f(0, 0);
-            GL.glTexCoord2f(0f, 1f);
-            GL.glVertex2f(0, 1f);
-            GL.glTexCoord2f(1f, 1f);
-            GL.glVertex2f(1f, 1f);
-            GL.glTexCoord2f(1f, 0f);
-            GL.glVertex2f(1f, 0);
-        } GL.glEnd();
+           // _xpos = -500;
+            //_zpos = -500;
+            GL.glPushAttrib(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT |
+                  GL.GL_ENABLE_BIT | GL.GL_POLYGON_BIT | GL.GL_STENCIL_BUFFER_BIT);
+            GL.glDisable(GL.GL_LIGHTING);    // Выключим свет
+            GL.glDepthMask(0);     // Выключим запись в буфер глубины
+            GL.glDepthFunc(GL.GL_LEQUAL);
+            GL.glEnable(GL.GL_STENCIL_TEST); // Включим тест буфера трафарета
+            GL.glColorMask(0, 0, 0, 0); // Не рисовать в буфер цвета
+            GL.glStencilFunc(GL.GL_ALWAYS, 1, 0xFFFFFFFF);
 
-        GL.glDepthMask(1);
+             GL.glFrontFace( GL.GL_CCW );
+      // Включить визуализацию цветового буфера для всех компонент
+             GL.glFrontFace(GL.GL_CW);
+             GL.glStencilOp(GL.GL_KEEP, GL.GL_KEEP, GL.GL_DECR);
+            GL.glColorMask( 1, 1,1, 1 );
+ 
+            // Нарисовать теневой прямоугольник на весь экран
+            GL.glColor4f( 0.0f, 0.0f, 0.0f, 0.4f );
+            GL.glEnable( GL.GL_BLEND );
+            GL.glBlendFunc( GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA );
+            GL.glStencilFunc( GL.GL_NOTEQUAL, 0, 0xFFFFFFFF );
+            GL.glStencilOp( GL.GL_KEEP, GL.GL_KEEP, GL.GL_KEEP );
+            GL.glPushMatrix();
+            GL.glLoadIdentity();
+            GL.glBegin( GL.GL_TRIANGLE_STRIP );
+            GL.glVertex3f(-0.1f, 0.1f,-0.10f);
+            GL.glVertex3f(-0.1f,-0.1f,-0.10f);
+            GL.glVertex3f( 0.1f, 0.1f,-0.10f);
+            GL.glVertex3f( 0.1f,-0.1f,-0.10f);
+            GL.glEnd();
+            GL.glPopMatrix();
+            GL.glPopAttrib();
+        }
 
-        GL.glPopMatrix();
-        GL.glMatrixMode(GL.GL_PROJECTION);
-        GL.glPopMatrix();
-        GL.glMatrixMode(GL.GL_MODELVIEW);
-    }
+        public void InitGl()
+        {
+            GL.glEnable(GL.GL_TEXTURE_2D);                                  // Enable Texture Mapping
+            GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE);						// Set The Blending Function For Translucency
+            GL.glShadeModel(GL.GL_SMOOTH);									// Enable Smooth Shading
+            GL.glClearColor(0.1f, 0.4f, 3.9f, 0f);						    // Black Background
+            GL.glClearDepth(1.0f);											// Depth Buffer Setup
+            GL.glEnable(GL.GL_DEPTH_TEST);									// Enables Depth Testing
+            GL.glDepthFunc(GL.GL_LESS);										// The Type Of Depth Testing To Do
+            GL.glHint(GL.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST);		// Really Nice Perspective Calculations
+        }
+
+        public void DropShadow(Vertex vertex)
+        {
+            foreach (var triangle in _sector1.Triangles)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    var vertex1 = triangle.Vertex[j];
+                    var vertex2 = triangle.Vertex[(j+1)%3];
+
+                    var vertex3 = new Vertex((vertex1.X - vertex.X) * 1000,
+                        (vertex1.Y - vertex.Y) * 1000, (vertex1.Z - vertex.Z) * 1000,0,0);
+                    var vertex4 = vertex3 = new Vertex((vertex2.X - vertex.X) * 1000,
+                        (vertex2.Y - vertex.Y) * 1000, (vertex2.Z - vertex.Z) * 1000, 0, 0);
+                    GL.glBegin(GL.GL_TRIANGLE_STRIP);
+                    GL.glVertex3f(vertex1.X, vertex1.Y, vertex1.Z);
+                    GL.glVertex3f(vertex1.X + vertex3.X, vertex1.Y + vertex3.Y, vertex1.Z + vertex3.Z);
+                    GL.glVertex3f(vertex2.X, vertex2.Y, vertex2.Z);
+                    GL.glVertex3f(vertex2.X + vertex4.X, vertex2.Y + vertex4.Y, vertex2.Z + vertex4.Z);
+                    GL.glEnd();
+                }
+
+            }
+            
+        }
+//        public void DrawBackground()
+//        {
+//            GL.glMatrixMode(GL.GL_PROJECTION);
+//            GL.glPushMatrix();
+//            GL.glOrtho(0, 1, 0, 1, 0, 1);
+//
+//            GL.glMatrixMode(GL.GL_MODELVIEW);
+//            GL.glPushMatrix();
+//            GL.glLoadIdentity();
+//
+//            // No depth buffer writes for background.
+//            GL.glDepthMask(0);
+//
+//            GL.glBindTexture(GL.GL_TEXTURE_2D, Textures.SkyTexture.TextureBytes[0]);
+//            GL.glBegin(GL.GL_QUADS);
+//            {
+//                GL.glTexCoord2f(0f, 0f);
+//                GL.glVertex2f(0, 0);
+//                GL.glTexCoord2f(0f, 1f);
+//                GL.glVertex2f(0, 1f);
+//                GL.glTexCoord2f(1f, 1f);
+//                GL.glVertex2f(1f, 1f);
+//                GL.glTexCoord2f(1f, 0f);
+//                GL.glVertex2f(1f, 0);
+//            } GL.glEnd();
+//
+//            GL.glDepthMask(1);
+//
+//            GL.glPopMatrix();
+//            GL.glMatrixMode(GL.GL_PROJECTION);
+//            GL.glPopMatrix();
+//            GL.glMatrixMode(GL.GL_MODELVIEW);
+//        }
         private float _heading;
         private float _xpos;
         private float _zpos;
@@ -80,26 +153,29 @@ namespace StreetView.OpenGL.Controls
             _listTextures.Add(Textures.GreeTexture);
             _listTextures.Add(Textures.BeigeTexture);
             _listTextures.Add(Textures.WhiteTexture);
-            DrawBackground();
+            _listTextures.Add(Textures.SkyTexture);
+            //DrawBackground();
         }
 
         public void SetupWorld()
         {
-            var building = new Building(0, 0);
+            //var building = new Building(0, 0);
             var lenskya3 = new Building(-50, 50);
             var ground = new Ground();
             //var brezhnevka = new BrezhnevkaBlock(0, -50, false, 9);
-            Lenskaya1 lenskaya1 = new Lenskaya1(0, -50);
+            var lenskaya1 = new Lenskaya1(0, -50);
             var lenskaya2 = new Lenkaya2(-60,-50);
             var lenskaya4 = new Lenskaya4(-15, -50);
             var lenskaya6 = new Lenskaya6(70, -70, 15);
-            _sector1.Triangles.AddRange(lenskaya6.GetPolygons());
+            var sky = new Sky();
+            _sector1.Triangles.AddRange(sky.GetPolygons());
+             _sector1.Triangles.AddRange(lenskaya6.GetPolygons());
             _sector1.Triangles.AddRange(lenskaya4.GetPolygons());
             _sector1.Triangles.AddRange(lenskaya2.GetPolygons());
             _sector1.Triangles.AddRange(lenskaya1.GetPolygons());
             _sector1.Triangles.AddRange(lenskya3.GetPolygons());
             _sector1.Triangles.AddRange(ground.GetPolygons());
-            _sector1.Triangles.AddRange(building.GetPolygons());
+           // _sector1.Triangles.AddRange(building.GetPolygons());
         }
 
         public override void glDraw()
@@ -112,8 +188,11 @@ namespace StreetView.OpenGL.Controls
 
             GL.glTranslatef(-_xpos -25,  - 1.25f, -_zpos +20);//камера
 
+            InitGl();
             foreach (Texture currentTexture in _listTextures)
                     DrawTrianglesWithTexture(currentTexture);
+            InitShadows();
+            DropShadow(new Vertex(499,499,499,0,0));
         }
 
         public void DrawTrianglesWithTexture(Texture currentTexture)
---
+++ StreetView/OpenGL/StreetElements/Sky.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StreetView.OpenGL.StreetElements
+{
+    class Sky : OpenGLObject
+    {
+        public Sky()
+        {
+            var rectangle = new Rectangle(-500f, 0, -500f, 0, 1000f, 1000f, 5f, 5f, Textures.SkyTexture);
+            OpenGLObjects.Add(rectangle);
+            rectangle = new Rectangle(-500f, 0, -500f, 1000, 1000f, 0, 5f, 5f, Textures.SkyTexture);
+            OpenGLObjects.Add(rectangle);
+            rectangle = new Rectangle(-500f, 0, 500f, 1000, 1000f, 0, 5f, 5f, Textures.SkyTexture);
+            OpenGLObjects.Add(rectangle);
+            rectangle = new Rectangle(500f, 0, -500f, 0, 1000f, 1000, 5f, 5f, Textures.SkyTexture);
+            OpenGLObjects.Add(rectangle);
+            rectangle = new Rectangle(-500f, 1000f, -500f, 1000f, 0, 1000f, 5f, 5f, Textures.SkyTexture);
+            OpenGLObjects.Add(rectangle);
+        }
+    }
+}
